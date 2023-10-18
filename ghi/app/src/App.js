66f840f39b39c6dccc67398a1f3fe179775bf9dc
Jake <jakeenger@yahoo.com>
2023-10-18 12:05:28 -0700

    added hatform.hatlist, modified nav.js and routes working in app.js

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
 
+// Import your HatForm and HatList components here
+import HatForm from './Hatform';
+import HatList from './hatlist';
+
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +13,10 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          {/* Define a route for HatForm */}
+          <Route path="/hats/new" element={<HatForm />} />
+          {/* Define a route for HatList */}
+          <Route path="/hats" element={<HatList />} />
         </Routes>
       </div>
     </BrowserRouter>
